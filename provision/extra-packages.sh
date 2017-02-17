b1eb1af1cf0c7bd3974ac7662a417fa4f98ccf53
Jay Luker <jay_luker@harvard.edu>
2017-02-17 07:25:07 -0500

    install backport of openjdk-8 from ppa repo and setup alternatives path

--- provision/extra-packages.sh
+++ provision/extra-packages.sh
@@ -2,4 +2,12 @@
 
 echo "==> Install extra packages"
 
-apt-get -yq install htop nmap traceroute silversearcher-ag screen tmux iotop mytop pv nethogs autofs5 curl dkms gzip libglib2.0-dev maven mediainfo mysql-client nodejs nodejs-dev npm openjdk-7-jdk openjdk-7-jre postfix python-pip rsyslog-gnutls run-one tesseract-ocr
+# install ppa repo for java8 backport
+apt-get -yq install software-properties-common
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+
+apt-get -yq install htop nmap traceroute silversearcher-ag screen tmux iotop mytop pv nethogs autofs5 curl dkms gzip libglib2.0-dev maven mediainfo mysql-client nodejs nodejs-dev npm openjdk-8-jdk openjdk-8-jre postfix python-pip rsyslog-gnutls run-one tesseract-ocr
+
+# set java8 to be the default alternative
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
